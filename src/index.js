043528a5742cc11688449abbe59b994f041e351a
Hanna Odnosum <odnosum.anna@gmail.com>
2023-01-25 15:35:26 +0200

    Added backgroun image and changed font size

--- src/index.js
+++ src/index.js
@@ -70,10 +70,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "017d56650cd168d68067850318775d43";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
